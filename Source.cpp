3b3a4cd9e0f322808cea76b3fa440d4c1eb22c6f
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 18:18:25 -0400

    Completed isSortedDecreasing

--- Source.cpp
+++ Source.cpp
@@ -5,21 +5,38 @@ using namespace std;
 bool isSortedIncreasing(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         if (arr[i] > arr[i + 1]) {
+            return false;  
+        }
+    }
+    return true;  
+}
+
+bool isSortedDecreasing(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] < arr[i + 1]) {
             return false; 
         }
     }
-    return true; 
+    return true;  
 }
 
 int main() {
     const int size = 5;
-    int exampleArray[size] = { 1, 3, 5, 7, 9 }; 
+    int exampleArrayIncreasing[size] = { 1, 3, 5, 7, 9 }; 
+    int exampleArrayDecreasing[size] = { 9, 7, 5, 3, 1 };  
 
-    if (isSortedIncreasing(exampleArray, size)) {
+    if (isSortedIncreasing(exampleArrayIncreasing, size)) {
         cout << "The array is sorted in increasing order." << endl;
     }
     else {
         cout << "The array is not sorted in increasing order." << endl;
+    }
+
+    if (isSortedDecreasing(exampleArrayDecreasing, size)) {
+        cout << "The array is sorted in decreasing order." << endl;
+    }
+    else {
+        cout << "The array is not sorted in decreasing order." << endl;
     }
 
     return 0;
