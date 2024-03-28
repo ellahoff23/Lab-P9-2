c0efad9026c066b0277c340843773411c3c59b85
Ella Hoffman <e-hoffman.2@onu.edu>
2024-03-28 18:26:18 -0400

    Completed hasAdjecentDuplicates

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool isSortedIncreasing(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-            return false;  
+            return false; 
         }
     }
     return true;  
@@ -17,13 +17,23 @@ bool isSortedDecreasing(int arr[], int size) {
             return false; 
         }
     }
-    return true;  
+    return true; 
+}
+
+bool hasAdjacentDuplicates(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            return true; 
+        }
+    }
+    return false; 
 }
 
 int main() {
     const int size = 5;
-    int exampleArrayIncreasing[size] = { 1, 3, 5, 7, 9 }; 
+    int exampleArrayIncreasing[size] = { 1, 3, 5, 7, 9 };  
     int exampleArrayDecreasing[size] = { 9, 7, 5, 3, 1 };  
+    int exampleArrayDuplicates[size] = { 2, 4, 6, 6, 8 };  
 
     if (isSortedIncreasing(exampleArrayIncreasing, size)) {
         cout << "The array is sorted in increasing order." << endl;
@@ -37,6 +47,13 @@ int main() {
     }
     else {
         cout << "The array is not sorted in decreasing order." << endl;
+    }
+
+    if (hasAdjacentDuplicates(exampleArrayDuplicates, size)) {
+        cout << "The array has adjacent duplicates." << endl;
+    }
+    else {
+        cout << "The array does not have adjacent duplicates." << endl;
     }
 
     return 0;
